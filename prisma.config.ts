cb139efa69cfb84239ca40a9406aae1b66eef03b
Seeeeego <k.s20030527@gmail.com>
2026-09-07 16:37:57 +0900

    環境構築

---
+++ prisma.config.ts
@@ -0,0 +1,12 @@
+import "dotenv/config";
+import { defineConfig } from "@prisma/config";
+
+export default defineConfig({
+  schema: "prisma/schema.prisma",     // 設計図の場所
+  migrations: {
+    path: "prisma/migrations",        // マイグレーション履歴の保存場所
+  },
+  datasource: {
+    url: process.env["DATABASE_URL"], // DB への接続 URL(環境変数から取得)
+  },
+});
